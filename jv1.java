dd4d3d57872f9f10efae9ab85dcf488ba974812e
rulanr <79989441+rulanr@users.noreply.github.com>
2021-03-08 00:30:15 +0700

    Add files via upload

---
+++ jv1.java
@@ -0,0 +1,17 @@
+package java1;
+
+public class jv1 {
+
+	public static void main(String[] args) {
+		System.out.println("java1");
+        int a = -5+8*6;
+        int b = (55+9)%9;
+        int c = 20+-3*5/8;
+        int d = 5+15/3*2-8%3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+	}
+
+}
