95410c4636f106587cefaec5d25c2da65496e371
rulanr <79989441+rulanr@users.noreply.github.com>
2021-04-08 21:06:40 +0700

    Create libsLide6.java

---
+++ Pertemuan 5/libsLide6.java
@@ -0,0 +1,29 @@
+// package praktikum5;
+
+public class libslide6 {
+    private static char predikat;
+    static char hitung(int nilai){
+        if(nilai >= 85){
+            predikat = 'A';
+        }else if((nilai>=70) && (nilai<85)){
+            predikat = 'B';
+        }else if((nilai>=60) && (nilai<70)){
+            predikat = 'C';
+        }else if((nilai>=50) && (nilai<60)){
+            predikat = 'D';
+        }else if(nilai<50){
+            predikat = 'E';
+        }
+        return predikat;
+    }
+
+    public static void daftarnilai(int[] nilai, int tot) {
+        System.out.println();
+        System.out.println("Daftar Nilai = ");
+
+        for (int j = 0; j < tot; j++) {
+            System.out.printf("Nilai %d, nilai hurufnya adalah %s", nilai[j], hitung(nilai[j]));
+            System.out.println();
+        }
+    }   
+}
